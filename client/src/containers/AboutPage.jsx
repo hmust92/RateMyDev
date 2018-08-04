76456c6b19888719ef21458130fdfa6b9071a420
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-08-04 13:05:58 -0500

    about styling

--- client/src/containers/AboutPage.jsx
+++ client/src/containers/AboutPage.jsx
@@ -17,39 +17,37 @@ class AboutPage extends React.Component {
         </div>
 
         <h1>Meet the Team</h1>
-
+          <div className="cardsContainer">
         <div className="row">
   <div className="column">
     <div className="card">
       <img src="/images/dkd.jpeg" alt="David" style={{width: '100%'}} />
-      <div className="container">
+      <div className="container aboutContainer">
         <h2>David Daniels</h2>
         <p className="title">UX Developer</p>
-        <p>Has some ideas. Feels strongly about them. Likes to work.</p>
+        <p className="details">Has some ideas. Feels strongly about them. Likes to work.</p>
         <p>daviddaniels@gmail.com</p>
         <p><button><a href="mailto:daviddaniels@gmail.com">Contact</a></button></p>
       </div>
     </div>
   </div>
   <div className="column">
-    <div className="card">
+    <div className="card aboutCard">
       <img src="/images/paulrose.jpeg" alt="Paul" style={{width: '100%'}} />
-      <div className="container">
-        <h2>Paul Rose</h2>
+      <div className="container aboutContainer">        <h2>Paul Rose</h2>
         <p className="title">Full Stack Developer</p>
-        <p>Thought deeply about how it all connects. Building things all the time.</p>
+        <p className="details">Thought deeply about how it all connects. Building things all the time.</p>
         <p>prose2334@gmail.com</p>
         <p><button><a href="mailto:prose2334@gmail.com">Contact</a></button></p>
       </div>
     </div>
   </div>
   <div className="column">
-    <div className="card">
+    <div className="card aboutCard">
       <img src="/images/hasanmustafa.jpeg" alt="Hasan" style={{width: '100%'}} />
-      <div className="container">
-        <h2>Hasan Mustafa</h2>
+      <div className="container aboutContainer">        <h2>Hasan Mustafa</h2>
         <p className="title">Full Stack Developer</p>
-        <p>Deep into the documentation, figuring out how the stuff works. Finding the necessary tools.</p>
+        <p className="details">Deep into the documentation, figuring out how the stuff works. Finding the necessary tools.</p>
         <p>hmust92@gmail.com</p>
         <p><button><a href="mailto:hmust92@gmail.com">Contact</a></button></p>
         {/* <p><button className="button">Contact</button></p> */}
@@ -57,31 +55,29 @@ class AboutPage extends React.Component {
     </div>
   </div>
   <div className="column">
-    <div className="card">
+    <div className="card aboutCard">
       <img src="/images/sherryyang.jpeg" alt="Sherry" style={{width: '100%'}} />
-      <div className="container">
-        <h2>Sherry Yang</h2>
+      <div className="container aboutContainer">        <h2>Sherry Yang</h2>
         <p className="title">UI</p>
-        <p>Anticipates the obstacles. Offers an alternative view.</p>
+        <p className="details">Anticipates the obstacles. Offers an alternative view.</p>
         <p>sharebear31496@gmail.com</p>
         <p><button><a href="mailto:sharebear31496@gmail.com">Contact</a></button></p>
       </div>
     </div>
   </div>
   <div className="column">
-    <div className="card">
+    <div className="card aboutCard">
       <img src="/images/ec.jpeg" alt="Elaulas" style={{width: '100%'}} />
-      <div className="container">
-        <h2>Elaulas Cristiano</h2>
+      <div className="container aboutContainer">        <h2>Elaulas Cristiano</h2>
         <p className="title">Subject Matter Expert (SME)</p>
-        <p>Brings a thoughtful perspective to the problem at hand.</p>
+        <p className="details">Brings a thoughtful perspective to the problem at hand.</p>
         <p>eelaulas@gmail.com</p>
         <p><button><a href="mailto:elaulas@gmail.com">Contact</a></button></p>
       </div>
     </div>
   </div>
 </div>
-
+</div>
       </div>
     );
   }
