1a757555f9cf9b202223139c5560e775a9dd6648
xxsherryyy <sharebear31496@gmail.com>
2018-08-08 19:51:41 -0500

    footer

--- client/src/components/davidcomponents/Footer/Footer.js
+++ client/src/components/davidcomponents/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Footer.css";
-
+  
 const Footer = () => (
   <footer className="footer">
-    <span>Rate That Dev &#169; 2018 || <a href="https://github.com/hmust92/react-passport-example">GitHub</a></span>
+    <span>Rate That Dev &#169; 2018 || <a href="https://github.com/hmust92/ratethatdev">GitHub</a></span>
   </footer>
 );
 
