09655468c4637d4076d9253fdd3e42ae4eeebdd3
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-08-13 22:55:19 -0500

    ever closer skill styling

--- client/src/components/TechnicalSkills.jsx
+++ client/src/components/TechnicalSkills.jsx
@@ -25,10 +25,11 @@ const TechnicalSkills = props => (
                             <p className="skillTagP">{skill.technicalTag}</p>
                             <div className="w3-light-grey w3-round-xlarge w3-small">
                                 <div className="w3-container w3-center w3-round-xlarge w3-indigo" style={{width: `${convertToPercent(skill.technicalPoints)}%`}}>{skill.technicalPoints}</div>,
+                                </div>
                                 <div className="w3-light-grey w3-round-xlarge w3-small">
                                 <div className="w3-container w3-center w3-round-xlarge w3-red" style={{width: `${convertToPercent(skill.technicalPoints)}%`}}>{skill.technicalPoints}</div>
                             </div>
-                            </div>
+                            
                         </div>
                     ))}
             </div>            
